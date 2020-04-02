fbb299693aa01fe971cb7a4508933384d2d64657
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-04-02 22:08:58 +0530

    Updates

---
+++ precache-manifest.2b98ab3ed4ec409b7162917c60a58790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c8958af5119ebbc2b14bb2a1e29279",
+    "url": "/WhatToDo/index.html"
+  },
+  {
+    "revision": "1cb0fecebcfb7999b5d4",
+    "url": "/WhatToDo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fda7d618a54b820edec5",
+    "url": "/WhatToDo/static/css/main.669adc13.chunk.css"
+  },
+  {
+    "revision": "1cb0fecebcfb7999b5d4",
+    "url": "/WhatToDo/static/js/2.1a28a952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatToDo/static/js/2.1a28a952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda7d618a54b820edec5",
+    "url": "/WhatToDo/static/js/main.401ce3cb.chunk.js"
+  },
+  {
+    "revision": "0465113ff86c83bafa41",
+    "url": "/WhatToDo/static/js/runtime-main.fe04dc16.js"
+  }
+]);
